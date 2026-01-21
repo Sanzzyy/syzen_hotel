678d5ee0f8b63862d46beebd4d86bfe1c847996f
Sanzzyy <sajidizzulhaqz@gmail.com>
2026-01-21 15:18:43 +0700

    fix: update gemini model name v2

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ mongoose
 // Konfigurasi AI
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 // Gunakan model yang stabil
-const model = genAI.getGenerativeModel({ model: "gemini-pro-latest" });
+const model = genAI.getGenerativeModel({ model: "gemini-flash-latest" });
 
 // Route Utama
 app.get("/", (req, res) => res.send("Backend Syzen Hotel Siap (Vercel)!"));
